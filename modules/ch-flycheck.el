6117c956176f0380e4c3f11360b245c0a5d7c68d
Cristian Hancila <cristianh@fb.com>
2014-10-03 23:21:09 -0700

    wip

--- modules/ch-flycheck.el
+++ modules/ch-flycheck.el
@@ -11,13 +11,7 @@
   "Configure some stuff for flycheck when in elisp mode."
   (setq flycheck-emacs-lisp-load-path load-path))
 
-(defun ch-configure-flycheck-cpp ()
-  "Configure some stuff for flycheck when in elisp mode."
-  (setq flycheck-gcc-language-standard "c++11")
-  (setq flycheck-clang-language-standard "c++11"))
-
 (add-hook 'emacs-lisp-mode-hook 'ch-configure-flycheck-elisp)
-(add-hook 'c++-mode-hook 'ch-configure-flycheck-cpp)
 
 (global-flycheck-mode t)
 
