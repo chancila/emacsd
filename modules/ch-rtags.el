81f79d6e55ce82407043abeb2ee88985373ae26a
Cristian hancila <chancila@gmail.com>
2016-02-11 00:47:52 -0800

    wip

---
+++ modules/ch-rtags.el
@@ -0,0 +1,10 @@
+;;; ch-rtags.el --- Rtags module
+;;; Commentary:
+;;; Code:
+(require 'ch-packages)
+(ch-require-package 'rtags)
+
+(require 'rtags)
+
+(provide 'ch-rtags)
+;;; ch-rtags.el ends here
