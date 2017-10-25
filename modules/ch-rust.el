106662d64cf9689857852bd063f19357d76baec3
Cristian Hancila <cristianh@turbo-dumpster-fire.dhcp.thefacebook.com>
2017-10-25 11:15:11 -0700

    wip

--- modules/ch-rust.el
+++ modules/ch-rust.el
@@ -2,21 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 (require 'ch-packages)
-(require 'ch-company)
 (require 'ch-flycheck)
 
-(ch-require-package 'company-racer)
 (ch-require-package 'flycheck-rust)
-(ch-require-package 'racer)
 (ch-require-package 'rust-mode)
-
-(with-eval-after-load 'company
-  (add-to-list 'company-backends 'company-racer))
 
 (add-hook 'rust-mode-hook
           '(lambda ()
              (eldoc-mode)
-             (racer-mode)
              (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
 
 (provide 'ch-rust)
